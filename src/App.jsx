600d5b39bb99ecbf7b9cb187f3cfad3fd96ca002
sweetkurut <devxwc@gmail.com>
2025-02-09 01:34:50 +0600

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
-
 function App() {
-  useEffect(() => {
-    tg.ready();
-  }, []);
-
-  const onClose = () => {
-    tg.close();
-  };
-
   return (
     <>
-      <h1>Hello dalbaeb</h1>
-      <button onClick={onClose}>Закрыть</button>
+      <h1>Hello</h1>
     </>
   );
 }
